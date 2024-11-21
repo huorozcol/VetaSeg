8484cbfcb4af316eaa4100f8507114af4c80b65b
Hugo <huorozcol@gmail.com>
2024-11-20 21:06:15 -0500

    modificacion 313

--- compare_vid_bd.py
+++ compare_vid_bd.py
@@ -1,4 +1,5 @@
 from time import sleep
+from trace import Trace
 
 import cv2
 from numpy.ma.core import shape
@@ -8,7 +9,7 @@ from numpy.ma.core import shape
 from crud import *
 from simple_facerec import SimpleFacerec
 import time
-
+import numpy as np
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
@@ -19,22 +20,34 @@ rojo = (0,0,255)
 verde = ( 0, 140, 13 )
 blanco = (255,250,250)
 
+def zoomframe(frame, scale):
 
-def comparar_faces(*args):
+    width = int(frame.shape[1]*scale)
+    heigth = int(frame.shape[0] * scale)
+    dimensions = (width,heigth)
+    # cv2.imshow('Zoom',frame)
+    # cv2.waitKey(1)
+    return cv2.resize(frame,dimensions, interpolation=cv2.INTER_AREA)
+
+
+def comparar_faces(scale, *args):
 
     cam_n = 1
     #print(f'frames enviados {len(args)}')
     for frame in args:
+
         if frame is not None:
+            frame = zoomframe(frame, scale)
             face_locations, face_names = sfr.detect_known_faces(frame)
             for face_loc, name in zip(face_locations, face_names):
-                name = name
+                name = name[:40]
                 # print(face_loc)
                 y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
                 if name == 'Visitante':
                     color = rojo
                 else:
                     color = verde
+                    print('Bienvenido: ', name)
                     #descomentar para que funcione, yo no la puedo descomentar porque no tengo el dispositvo usb y me va a tirar error
                     #open_door.on_relay(1)
 
@@ -47,9 +60,8 @@ def comparar_faces(*args):
                     break
 
             if cam_n == 1:
-                # cv2.imshow("Reconocimiento facial ITM - Prueba - ", frame)
+                # cv2.imshow("Reconocimiento facial VETA Seguridad- ", frame)
                 # cv2.waitKey(1)
                 cont_frame = +1
             else:
                 continue
-
--- gui.py
+++ gui.py
@@ -2,14 +2,17 @@
 import cv2
 import tkinter as tk
 from threading import Thread
-from tkinter import Entry, Scale, Checkbutton, IntVar, Button
-import csv
+from tkinter import Entry, Scale, IntVar
 from compare_vid_bd import comparar_faces
 from config import create_direct
-from PIL import ImageTk, Image
-import simple_facerec
 import crud
-import time
+import numpy as np
+
+
+
+def downl_reporte():
+     crud.ult_registros()
+
 
 class GUI:
     def __init__(self, winroot):
@@ -28,15 +31,19 @@ class GUI:
         self.var_cb_showcv2 = IntVar()
         self.winroot = winroot
         self.winroot.title("VETA Seguridad - Acceso Facial -")
-        self.fr_data_camara = tk.Frame(self.winroot, width=600, height=200, borderwidth=2, relief='groove').place(x=10,y=10)
-        self.ip_label = tk.Label(self.winroot, text='Ingrese la dirección de la cámaras:').place(x=15,y=15)
+        self.fr_data_camara = tk.Frame(self.winroot, width=600, height=300, borderwidth=2, relief='groove')
+        self.fr_data_camara.place(x=10, y=10)
+        self.ip_label = tk.Label(self.winroot, text='Ingrese la dirección de la cámaras:')
+        self.ip_label.place(x=15, y=15)
 
-        self.ip_label = tk.Label(self.winroot, text='Cámara 1:').place(x=15, y=35)
+        self.ip_label = tk.Label(self.winroot, text='Cámara 1:')
+        self.ip_label.place(x=15, y=35)
         self.txb_ip_camara1 = Entry(self.winroot, width=45)
         self.txb_ip_camara1.insert(0,"rtsp://hugo:solkaf2008@192.168.1.64:554")
         self.txb_ip_camara1.place(x=75,y=35)
 
-        self.ip_label = tk.Label(self.winroot, text='Cámara 2:').place(x=15, y=55)
+        self.ip_label = tk.Label(self.winroot, text='Cámara 2:')
+        self.ip_label.place(x=15, y=55)
         self.txb_ip_camara2 = Entry(self.winroot, width=45)
         self.txb_ip_camara2.insert(0, "rtsp://hugo:solkaf2008@192.168.1.164:554")
         self.txb_ip_camara2.place(x=75, y=55)
@@ -46,39 +53,36 @@ class GUI:
         self.bt_unplug_camara = (tk.Button(self.winroot, text='Desconectar', command=self.unplug_camara))
         self.bt_unplug_camara.config(state='disabled')
         self.bt_unplug_camara.place(x=475,y=105)
-        self.lb_framerate = tk.Label(self.winroot, text='Frames Skip ')
+        self.lb_framerate = tk.Label(self.winroot)
         self.lb_framerate.place(x=17,y=130)
-        self.sc_framerate = Scale(self.winroot, from_=1, to=30, orient=tk.HORIZONTAL)
+        self.sc_framerate = Scale(self.winroot, from_=1, to=30, orient=tk.HORIZONTAL, label='Frames Skip')
         self.sc_framerate.set(10)
-        self.sc_framerate.place(x=152, y=105)
+        self.sc_framerate.place(x=15, y=145)
         # self.chb_show_cv2 = tk.Checkbutton(self.winroot, state='normal', onvalue=1, offvalue=0, text='Ver Video', variable=self.var_cb_showcv2)
         # self.chb_show_cv2.select()
         # self.chb_show_cv2.place(x=290,y=130)
-        self.lb_framerate = tk.Label(self.winroot, text='Tiempo apertura ')
-        self.lb_framerate.place(x=17, y=159)
-        self.sc_timer_door = Scale(self.winroot, from_=1, to=5, orient=tk.HORIZONTAL)
+        # self.lb_framerate = tk.Label(self.winroot, text='Tiempo apertura ')
+        # self.lb_framerate.place(x=17, y=159)
+
+        self.sc_timer_door = Scale(self.winroot, from_=1, to=5, orient=tk.HORIZONTAL, label='Apertura')
         self.sc_timer_door.set(3)
-        self.sc_timer_door.place(x=152, y=145)
+        self.sc_timer_door.place(x=182, y=145)
+
+        self.zoom = Scale(self.winroot, from_=1, to=5, orient=tk.HORIZONTAL, label='Zoom', resolution=0.2)
+        self.zoom.set(3)
+        self.zoom.place(x=352, y=145)
 
         self.lb_stat_camara = tk.Label(self.winroot, text='')
-        self.lb_stat_camara.place(x=15,y=185)
+        self.lb_stat_camara.place(x=15,y=215)
         self.fr_data_personal = tk.Frame(self.winroot, width=600, height=200, borderwidth=2, relief='groove')
         self.fr_data_personal.place(x=10,y=270)
         self.lb_status_detect_face = tk.Label(self.fr_data_personal, text='')
-        self.bt_downl_reg = (tk.Button(self.fr_data_personal, text='Descargar Reporte', command=self.downl_reporte))
+        self.bt_downl_reg = (tk.Button(self.fr_data_personal, text='Descargar Reporte', command=downl_reporte))
         self.bt_downl_reg.place(x=20,y=10)
         self.lb_status_detect_face.place(x=15,y=15)
         self.ph_image_detected = tk.PhotoImage(file='')
         # self.lb_image_detected = tk.Label(self.fr_data_personal, image=self.downl_reporte())
         # self.lb_image_detected.place(x=200,y=10)
-
-    def downl_reporte(self):
-
-        rows = crud.ult_registros()
-
-
-
-
 
     def close_gui(self):
         print("Cerrando la ventana")
@@ -106,7 +110,7 @@ class GUI:
         self.lb_stat_camara.config(text="")
         if not self.flag_video_capture:
             print('Inicia capture videos')
-            #aqui es donde esta quemado el codigo.
+
             if  ip_camara1 is not None: self.cap1 = cv2.VideoCapture(ip_camara1)
             if  ip_camara2 is not None: self.cap2 = cv2.VideoCapture(ip_camara2)
             self.flag_video_capture = True
@@ -142,6 +146,7 @@ class GUI:
             self.show_frames()
             #################################
 
+
     def show_frames(self):
         if self.is_running and self.flag_video_capture:
             self.count_fr += 1
@@ -158,19 +163,20 @@ class GUI:
 
 
             if self.count_fr % self.sc_framerate.get() == 0 and self.ret1:
-                th_showcv2 = Thread(target=comparar_faces, args=(self.frame1,self.frame2,))
+                #comparar_faces(self.frame1,self.frame2)
+                th_showcv2 = Thread(target=comparar_faces, args=(self.zoom.get(),self.frame1,self.frame2,))
                 th_showcv2.run()
                 if self.var_cb_showcv2.get() == 1:
-                    pass
+                    self.show_cv2()
             self.ult_registros()
             self.lb_stat_camara.after(1, self.show_frames)
         else:
             self.unplug_camara()
 
-    def show_cv2(self, frame):
-
-        cv2.imshow('Camara Reconocimiento Facial - ITM -', self.frame1)
-        cv2.waitKey(1)
+    def show_cv2(self):
+        pass
+        # cv2.imshow('Camara Reconocimiento Facial - ITM -', self.frame1)
+        # cv2.waitKey(1)
 
     def unplug_camara(self):
         self.lb_status_detect_face.config(text="")
@@ -190,13 +196,16 @@ class GUI:
 
 
 if __name__ == '__main__':
+
     winroot = tk.Tk()
-def startAll():
+
+def startall():
     print('funcion start all')
     app = GUI(winroot)
     winroot.geometry('640x480')
     winroot.protocol("WM_DELETE_WINDOW", app.close_gui)
-th_startAll = Thread(target=startAll, args=(), daemon=True)
+    winroot.mainloop()
+#startall()
+th_startAll = Thread(target=startall, args=(), daemon=True)
 th_startAll.run()
 create_direct()
-winroot.mainloop()

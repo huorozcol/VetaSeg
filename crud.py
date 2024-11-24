e1473cb3f88169f6ac2205db7e26edb0e390153c
Hugo <huorozcol@gmail.com>
2024-11-24 17:01:51 -0500

    Improve csv report

--- crud.py
+++ crud.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 from conn import conn_op, conn_cl
 import time
@@ -7,22 +8,28 @@ def insert_face(name, cam_n):
     fecha = time.strftime("%Y-%m-%d %H:%M:%S")
     my_con = conn_op()
     my_cursor = my_con.cursor()
-    sql = "INSERT INTO registro (name, fecha, cam_n) VALUES(%s, %s, %s)"
+    sql = "INSERT INTO registro (nombre, fecha, camara) VALUES(%s, %s, %s)"
     vals = (name, fecha, cam_n)
     my_cursor.execute(sql, vals)
     my_con.commit()
     my_con.close()
 
 def ult_registros():
-    fecha = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    fecha = time.strftime("%Y-%m-%d-%H-%M-%S")
     my_con = conn_op()
     my_cursor = my_con.cursor()
+    path_desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
     sql = "SELECT * FROM face_recog.registro order by fecha desc"
     my_cursor.execute(sql)
-    result = my_cursor.fetchall()
-    fp = open(f'reporte{fecha}.csv', 'w')
-    myFile = csv.writer(fp)
-    myFile.writerow(result)
-    fp.close()
+    #result = my_cursor.fetchall()
+    # fp = open(f'./reporte{fecha}.csv', 'w')
+    # myFile = csv.writer(fp)
+    # myFile.writerow(result)
+    # fp.close()
+    with open(f'{path_desktop}\\reporte-{fecha}.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([i[0] for i in my_cursor.description])
+        writer.writerows(my_cursor.fetchall())
 
-    return  result
+    #return  result
